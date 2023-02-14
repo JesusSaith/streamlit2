b0a8b9bb80a5b59874d67516d7a0938ecf78c49a
SaithConD <yisuscond@gmail.com>
2023-02-14 10:17:32 -0600

    actualizacion search

--- search_names.py
+++ search_names.py
@@ -1,21 +1,21 @@
 import streamlit as st
 import pandas as pd
 
-st.title("search names")
+st.title("Search names")
 DATA_URL = "dataset.csv"
 
 @st.cache
 def load_data_byname(name):
-    data=pd.read_csv(DATA_URL)
-    filtered_data_byname=data[data ["name"].str.contains(name)]
+    data = pd.read_csv(DATA_URL)
+    filtered_data_byname = data[data["name"].str.contains(name)]
     return filtered_data_byname
 
 myname = st.text_input("nombre :")
-if(myname):
-    filtered_data_byname=load_data_byname(myname)
-    count_row=filterbyname.shape[0]
-    st.write(f"total names :{count_row}")
+if (myname):
+        filterbyname = load_data_byname(myname)
+        count_row = filterbyname.shape[0]
+        st.write(f"Total names: {count_row}")
 
-    st.dataframe(filterbyname)
+        st.dataframe(filterbyname)
 
 
