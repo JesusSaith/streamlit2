16bae8958aac96049093c30dc05d70099da0368a
SaithConD <yisuscond@gmail.com>
2023-02-14 10:04:41 -0600

    Streamlit

---
+++ ejercicio2.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title('my first app')
+st.header('my first app')
+
+st.write(""" 
+
+Nada e' normal contigo, baby, tú eres especial
+To's mis dolores como los supiste anestesiar
+Desde que te conocí vi tu potencial
+Flow fuera del planeta y tú, nave espacial
+
+
+""")
